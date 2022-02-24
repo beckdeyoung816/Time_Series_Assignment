5915569e8183837b693f419d07ac0a4cec34629d
beckdeyoung816 <beckdeyoung@gmail.com>
2022-02-24 17:20:52 +0100

    Switch to OOP

---
+++ Part 1/LLM.py
@@ -0,0 +1,267 @@
+import pandas as pd
+import numpy as np
+from scipy.optimize import minimize
+
+########## NOTATION
+
+# v_t = Prediction Error
+# F_t = var(v_t)
+
+# K_t = Kalman Gain
+
+# a_t / a_tt = mean(alpha_t) Filter State
+# p_t / p_tt = var(alpha_t)
+
+# u_t = Smoothing Error
+# D_t = var(u_t)
+
+# r_t = Smoothing Cumulant
+# N_t = var(r_t)
+
+# alpha_t = 
+# V_t =
+
+# e_hat_t = Smoothed Observation Disturbance / Error
+# h_hat_t = Smoothed Mean of the Disturbance / State Error
+
+
+# u_star_t = Observation Residual
+# r_star_t = State Residual
+# e_t = Standardised Prediction Errors
+
+
+class LLM:
+    def __init__(self, data: pd.DataFrame, var_e:float=None, var_h:float=None, a_1:float=None, P_1:float=None, ML_start:list=None):
+        """Initialize Local Level Model Object
+        Args:
+            data (pd.DataFrame): Time Series Data
+            var_e (float, optional): Variance of Epsilon. Defaults to None.
+            var_h (float, optional): Variance of Eta. Defaults to None.
+            a_1 (float, optional): Starting value for a_t. Defaults to None.
+            P_1 (float, optional): Starting value for P_t. Defaults to None.
+            ML_start (list, optional): Starting values for maximum likelihood estimation
+        """
+        # Assign final attributes of the data and the number of observations    
+        self.df = data
+        self.N = self.df.shape[0]
+        # If no variances are specified, then we estimate them through maximum likelihood
+        # a_1 and P_1 are implied in this case
+        if var_e is None or var_h is None:
+            if ML_start is None:
+                raise ValueError('Please specify ML_Start or var_e and var_h')         
+            self.res = self.Kalman_ML(ML_start)
+            self.var_e, self.var_h = self.res.x
+            
+        # If variances are specified, starting values for a_t and P_t must be specified too 
+        elif a_1 is None or P_1 is None:
+            raise ValueError('Please specify a_1 and/or P_1')
+        
+        # Otherwise, assign the given variances and starting values
+        else:
+            self.var_e = var_e
+            self.var_h = var_h
+            self.a_1 = a_1
+            self.P_1 = P_1
+    
+    def KF_filter(self, y_t: float, a_t: float, P_t: float, var_e: float, var_h: float):
+        """Kalman Filter at a single point in time given values at time t-1
+
+        Args:
+            y_t (float): True value
+            a_t (float): 
+            P_t (float): 
+            var_e (float): Variance of Epsilon
+            var_h (float): Variance of Eta
+
+        Returns:
+            _type_: New values at time t
+        """
+        # Kalman gain calculation
+        v_t = y_t - a_t
+        F_t = P_t + var_e
+        K_t = P_t / F_t
+
+        # State Update / Filtering Step
+        a_tt = a_t + (K_t * v_t)
+        P_tt = P_t * (1-K_t)
+
+        # Prediction Step
+        a_t = a_tt
+        P_t = P_tt + var_h
+
+        return a_tt, P_tt, a_t, P_t, v_t, F_t, K_t
+    
+    def Kalman_filter(self):
+        """Kalman Filter for the entire time series
+        """
+        # Initialize columns
+        self.df[['a_t','P_t', 'v_t','F_t','K_t']] = np.nan
+        
+        # Loop through each observation of y_t
+        for t, y_t in enumerate(self.df['y_t']):
+            # Initialize Values
+            if t == 0 :
+                a_t = self.a_1 
+                P_t = self.P_1
+            
+            # Apply the filter and save updated values for next iteration
+            a_tt, P_tt, a_t, P_t, v_t, F_t, K_t = self.KF_filter(y_t=y_t, a_t=a_t, P_t=P_t,
+                                                                 var_e=self.var_e, var_h=self.var_h)
+            
+            # Store output
+            self.df.loc[t, ['a_t','P_t','v_t','F_t','K_t']] = [a_tt, P_tt, v_t, F_t, K_t]
+
+    def state_smooth(self):
+        """Perform State Smoothing
+        """
+        # Create Helper variable L_t
+        self.df['L_t'] = 1 - self.df['K_t']
+        
+        # Initialize columns for recursion
+        self.df[['r_t','N_t']] = np.nan
+
+        # Smoothing goes in reverse (from t=n to t=1)
+        for t in reversed(range(self.N)):
+            if t == self.N-1: # Since the last observation has no 't+1', set it to 0
+                self.df.loc[t, ['r_t', 'N_t']] = [0,0]
+            else:
+            # Apply the formulas using t+1 when necessary
+                self.df.loc[t, 'N_t'] = (1 / self.df.loc[t+1, 'F_t']) + ((self.df.loc[t+1, 'L_t'] ** 2) * self.df.loc[t+1, 'N_t'])
+                self.df.loc[t, 'r_t'] = (self.df.loc[t+1, 'v_t'] / self.df.loc[t+1, 'F_t']) + \
+                                        (self.df.loc[t+1, 'L_t'] * self.df.loc[t+1, 'r_t'])
+                                        
+    def disturbance_smooth(self):
+        """Perform Disturbance Smoothing
+        """
+        # Initliaze columns 
+        self.df[['alpha_hat_t', 'V_t']] = np.nan
+        
+        # Set first value and then recurse
+        # We thus start at the second value for a_t and P_t
+        # These equations require r_{t-1} and N_{t-1} so we do not use the last values and start at first value
+        self.df.loc[0, 'alpha_hat_t'] = self.a_1
+        self.df.loc[1:,'alpha_hat_t'] = self.df.loc[1:, 'a_t'] + self.df.loc[1:, 'P_t'] * self.df.loc[:self.N, 'r_t']
+        
+        # Set first value then recurse
+        self.df.loc[0, 'V_t'] = self.P_1
+        self.df.loc[1:,'V_t'] = self.df.loc[1:, 'P_t'] - ((self.df.loc[1:, 'P_t'] ** 2) * self.df.loc[:self.N, 'N_t'])
+
+        # Smoothing Error and Variance
+        self.df['u_t'] = (self.df['v_t'] / self.df['F_t']) - (self.df['K_t'] * self.df['r_t'])
+        self.df['D_t'] = (1 / self.df['F_t']) + ((self.df['K_t'] ** 2) * self.df['N_t'])
+
+        # Smoothed Observation Error and State Error
+        self.df['e_hat_t'] = self.var_e * self.df['u_t']
+        self.df['h_hat_t'] = self.var_h * self.df['r_t']
+
+        # Variances and SDs of Observation and State Errors
+        self.df['var_e_hat_t'] = self.var_e - ((self.var_e ** 2) * self.df['D_t'])
+        self.df['sd_e_hat_t'] = np.sqrt(self.df['var_e_hat_t'])
+        
+        self.df['var_h_hat_t'] = self.var_h - ((self.var_h ** 2) * self.df['N_t'])
+        self.df['sd_h_hat_t'] = np.sqrt(self.df['var_h_hat_t'])
+    
+    def auxilary_residuals(self):
+        self.df['u_star_t'] = self.df['u_t'] / np.sqrt(self.df['D_t'])
+        self.df['r_star_t'] = self.df['r_t'] / np.sqrt(self.df['N_t'])
+        self.df['e_t'] = self.df['v_t'] / np.sqrt(self.df['F_t'])
+        
+    def get_90_conf_intervals(self, col:str, var:str):
+        """Calculate 90 percent Confidence Intervals for a given variable
+
+        Args:
+            col (str): Column name of the variable
+            var (str): Column name of the variance of the variable
+        """
+        self.df[col+'_upper_c'] = self.df[col] + 1.96 * np.sqrt(self.df[var])
+        self.df[col+'_lower_c'] = self.df[col] - 1.96 * np.sqrt(self.df[var])
+    def Kalman_ML(self, starting_vars):
+        def ML_fun(vars):
+            var_e = vars[0]
+            var_h = vars[1]
+            q = var_h/var_e
+            for t, y_t in enumerate(self.df['y_t'][1:]):
+                # Initialize Values
+                if t == 0 :
+                    a_t = self.df['y_t'][0] # Initialize at y1
+                    P_t = var_e + var_h
+                
+                # Apply the filter
+                a_tt, P_tt, a_t, P_t, v_t, F_t, K_t = self.KF_filter(y_t=y_t, a_t=a_t, P_t=P_t, 
+                                                            var_e=var_e, var_h=var_h)
+                
+                # Store output
+                self.df.loc[t, ['a_t','P_t','v_t','F_t','K_t']] = [a_tt, P_tt, v_t, F_t, K_t]
+            
+            self.df['F_star_t'] = self.df['F_t'] / var_e
+            var_e_hat = 1 / (self.N - 1) * np.sum(self.df['v_t'][1:] ** 2 / self.df['F_star_t'][1:])
+
+            return -self.N/2 * np.log(2 * np.pi) - (self.N - 1) / 2 - \
+                (self.N - 1) / 2 * np.log(var_e_hat) - \
+                    0.5 * np.sum(np.log(self.df['F_star_t'][1:]))
+                    
+        bnds = [(1e-10, None), (1e-10, None)]
+        results = minimize(ML_fun, starting_vars, method = 'L-BFGS-B', bounds = bnds)
+        return results
+    
+    def Kalman_ML_q(self, q0):
+        def ML_fun_q(q):
+            var_e = 1
+            var_h = q[0]
+            q = var_h/var_e
+            for t, y_t in enumerate(self.df['y_t'][1:]):
+                # Initialize Values
+                if t == 0 :
+                    a_t = self.df['y_t'][0] # Initialize at y1
+                    P_t = var_e + var_h
+                
+                # Apply the filter
+                a_tt, P_tt, a_t, P_t, v_t, F_t, K_t = self.KF_filter(y_t=y_t, a_t=a_t, P_t=P_t, 
+                                                            var_e=var_e, var_h=var_h)
+                
+                # Store output
+                self.df.loc[t, ['a_t','P_t','v_t','F_t','K_t']] = [a_tt, P_tt, v_t, F_t, K_t]
+            
+            self.df['F_star_t'] = self.df['F_t'] / var_e
+            var_e_hat = 1 / (self.N - 1) * np.sum(self.df['v_t'][1:] ** 2 / self.df['F_star_t'][1:])
+
+            return -self.N/2 * np.log(2 * np.pi) - (self.N - 1) / 2 - \
+                (self.N - 1) / 2 * np.log(var_e_hat) - \
+                    0.5 * np.sum(np.log(self.df['F_star_t'][1:]))
+                    
+        bnds = [(1e-10, None)]
+        results = minimize(ML_fun_q, [q0], method = 'L-BFGS-B', bounds = bnds)
+        return results
+    
+    
+    def Kalman_avg_ML(self, starting_vars):
+        def ML_avg_fun(vars):
+            var_e = vars[0]
+            var_h = vars[1]
+            q = var_h/var_e
+            
+            total_ll = 0
+            for t, y_t in enumerate(self.df['y_t'][1:]):
+                # Initialize Values
+                if t == 0 :
+                    a_t = self.df['y_t'][0] # Initialize at y1
+                    P_t = var_e + var_h
+                
+                # Apply the filter
+                a_tt, P_tt, a_t, P_t, v_t, F_t, K_t = self.KF_filter(y_t=y_t, a_t=a_t, P_t=P_t, 
+                                                                     var_e=var_e, var_h=var_h)
+                
+                # Store output
+                self.df.loc[t, ['a_t','P_t','v_t','F_t','K_t']] = [a_tt, P_tt, v_t, F_t, K_t]
+                
+                # Total Log likelihood
+                total_ll += -self.N/2 * np.log(2 * np.pi) - \
+                    (1/2) * np.sum(np.log(self.df['F_t'][1:])) - \
+                    (1/2) * np.sum(self.df['v_t'][1:] ** 2 * self.df['F_t'][1:])
+            
+            return total_ll / self.N # Average LL
+                    
+                    
+        bnds = [(1e-10, None), (1e-10, None)]
+        results = minimize(ML_avg_fun, starting_vars, method = 'L-BFGS-B', bounds = bnds)
+        return results
---
+++ Part 1/Part1_Figures.py
@@ -0,0 +1,186 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+
+from LLM import LLM
+    
+# %%
+def fig_1(data, ylim_0=None, xlim_0=None, ylim_2=None, ylim_3=None):
+    fig, axes = plt.subplots(2,2,figsize=(10,10))
+    axes = axes.ravel()
+
+    # Data and Filtered State
+    ## Observed Data
+    data.plot(ax = axes[0], x = 'x', y = 'y_t', linestyle = 'none', marker = '.', color = 'red',
+            ylim = ylim_0, xlim = xlim_0, legend = None)
+    ## Filtered State
+    data.plot(ax = axes[0], x = 'x', y = 'a_t', color = 'blue', legend = None)
+    ## Confidence Intervals
+    data.plot(ax = axes[0], x = 'x', y = 'a_t_upper_c', color = 'black', linewidth = 0.4, legend = None)
+    data.plot(ax = axes[0], x = 'x', y = 'a_t_lower_c', color = 'black', linewidth = 0.4, legend = None)
+
+    # Filtered State Variance Pt
+    data.plot(ax = axes[1], x = 'x', y = 'P_t', legend = None)
+
+    # Prediction Errors vt
+    data.plot(ax = axes[2], x = 'x', y = 'v_t', ylim = ylim_2, legend = None)
+    axes[2].axhline(y = 0, color = 'black', linestyle = ':') # Line at x axis
+
+    # Prediction Variance Ft
+    data.iloc[1:].plot(ax = axes[3], x='x', y='F_t', ylim=ylim_3, legend = None) # Ignore initialization
+
+    # Save Figure
+    plt.savefig('Figures/Fig_2_1.png', facecolor='w')
+    plt.show()
+
+def fig_2(data, ylim_0=None, xlim_0=None, ylim_1=None, ylim_3=None):
+    fig, axes = plt.subplots(2,2, figsize = (10,10))
+    axes = axes.ravel()
+
+    # Data and Smoothed State alpha_hat
+    ## Observed Data
+    data.plot(ax = axes[0], x = 'x', y = 'y_t', linestyle = 'none', marker = '.', color = 'red',
+            ylim = ylim_0, xlim = xlim_0, legend = None)
+    ## Smoothed State
+    data.iloc[1:].plot(ax = axes[0], x = 'x', y = 'alpha_hat_t', color = 'blue', legend = None)
+    ## Confidence Intervals
+    data.iloc[1:].plot(ax = axes[0], x = 'x', y = 'alpha_hat_t_upper_c', color = 'black', linewidth = 0.4, legend = None)
+    data.iloc[1:].plot(ax = axes[0], x = 'x', y = 'alpha_hat_t_lower_c', color = 'black', linewidth = 0.4, legend = None)
+
+    # Smoothed State Variance Vt
+    data.plot(ax = axes[1], x = 'x', y = 'V_t', ylim = ylim_1, legend = None)
+
+    # Smoothing Cumulant rt
+    data.plot(ax = axes[2], x = 'x', y = 'r_t', legend = None)
+    axes[2].axhline(y = 0, color = 'black', linestyle = ':') # Line at x axis
+    
+    # Smoothing Cumulant Variant Nt
+    data.plot(ax = axes[3], x = 'x', y = 'N_t', ylim = ylim_3, legend = None)
+
+    # Save Figure
+    plt.savefig('Figures/Fig_2_2.png', facecolor = 'w')
+    plt.show()
+
+def fig_3(data, ylim_0=None, xlim_0=None, ylim_2=None, xlim_2=None):
+    fig, axes = plt.subplots(2,2,figsize=(10,10))
+    axes = axes.ravel()
+
+    # Observation Error e hat
+    data.plot(ax = axes[0], x = 'x', y = 'e_hat_t', ylim = ylim_0, xlim = xlim_0, legend = None)
+    axes[0].axhline(y = 0, color = 'black')
+
+    # Observation Error Standard Deviation
+    data.plot(ax = axes[1], x = 'x', y = 'sd_e_hat_t', legend = None)
+
+    # State error h hat
+    data.plot(ax = axes[2], x = 'x', y = 'h_hat_t', ylim = ylim_2, xlim = xlim_2, legend = None)
+    axes[2].axhline(y = 0, color = 'black')
+
+    # State Error Standard Deviation
+    data.plot(ax = axes[3], x = 'x', y = 'sd_h_hat_t', legend = None)
+
+    # Save Figure
+    plt.savefig('Figures/Fig_2_3.png', facecolor = 'w')
+    plt.show()
+
+def fig_5(data):
+    fig, axes = plt.subplots(2,2,figsize=(10,10))
+    axes = axes.ravel()
+
+
+
+    # Save Figure
+    plt.savefig('Figures/Fig_2_5.png', facecolor = 'w')
+    plt.show()
+
+def fig_6(data):
+    fig, axes = plt.subplots(2,2,figsize=(10,10))
+    axes = axes.ravel()
+
+
+
+    # Save Figure
+    plt.savefig('Figures/Fig_2_6.png', facecolor = 'w')
+    plt.show()
+
+def fig_7(data, ylim_0=None, ylim_3=None, xlim_3=None):
+    fig, axes = plt.subplots(2,2,figsize=(10,10))
+    axes = axes.ravel()
+
+    # Standardized Residual
+    data.plot(ax = axes[0], x = 'x', y = 'e_t', ylim=ylim_0, legend = None)
+    axes[0].axhline(y = 0, color = 'black', linestyle = ':')
+
+    # Histogram plus estimated density
+    sns.histplot(ax = axes[1], data=data.iloc[2:], x = 'e_t', stat='density', kde=True, bins=17)
+
+    # Ordered residuals
+    sm.qqplot(data.loc[9:, 'e_t'], ax = axes[2], line ='45')
+
+    # Correlogram
+    correls = np.correlate(data['e_t'], data['e_t'], mode="full")
+    correls /= np.dot(data['e_t'], data['e_t'])
+
+    maxlags = 10
+    N_OBS = data.shape[0]
+    lags = np.arange(-maxlags, maxlags + 1)
+    correls = correls[N_OBS - 1 - maxlags:N_OBS + maxlags]
+
+    axes[3].vlines(lags, [0], correls, linewidth = 15)
+    axes[3].axhline()
+    axes[3].set_ylim(ylim_3)
+    axes[3].set_xlim(xlim_3)
+
+    # Save Figure
+    plt.savefig('Figures/Fig_2_7.png', facecolor = 'w')
+    plt.show()
+
+def fig_8(data, ylim_0=None, ylim_2=None):
+    fig, axes = plt.subplots(2,2,figsize=(10,10))
+    axes = axes.ravel()
+
+    # Observation Residual ut^*
+    data.plot(ax = axes[0], x = 'x', y = 'u_star_t', ylim=ylim_0, legend = None)
+    axes[0].axhline(y = 0, color = 'black', linestyle = ':')
+
+    # Histogram and estimated density for ut^*
+    sns.histplot(ax = axes[1], data=data.iloc[2:], x = 'u_star_t', stat='density', kde=True, bins=17, binwidth = 0.4)
+
+
+    # State Residual rt^*
+    data.plot(ax = axes[2], x = 'x', y = 'r_star_t', ylim=ylim_2, legend = None)
+    axes[2].axhline(y = 0, color = 'black', linestyle = ':')
+
+    # Histogram and estimated density for rt^*
+    sns.histplot(ax = axes[3], data=data.iloc[2:], x = 'r_star_t', stat='density', kde=True, bins=17, binwidth = 0.4)
+
+    # Save Figure
+    plt.savefig('Figures/Fig_2_8.png', facecolor = 'w')
+
+# %%
+#data = pd.read_csv('../Data/AirPassengers.csv')
+data = pd.read_excel('../Data/Nile.xlsx', names = ['x', 'y_t'])
+nile_llm = LLM(data=data, var_e=15099, var_h=1469.1, a_1=0, P_1=10**7)
+# %%
+nile_llm.Kalman_filter()
+nile_llm.state_smooth()
+nile_llm.disturbance_smooth()
+nile_llm.auxilary_residuals()
+
+# Confidence intervals
+nile_llm.get_90_conf_intervals('a_t', 'P_t')
+nile_llm.get_90_conf_intervals('alpha_hat_t', 'V_t')
+
+# Generate and Save Figures
+fig_1(nile_llm.df, ylim_0=(450,1400), xlim_0=(1865,1975),ylim_2=(-450,450), ylim_3=(20000,32500))
+fig_2(nile_llm.df, ylim_0=(450,1400), xlim_0=(1865,1975),ylim_1=(2200, 4100), ylim_3=(6e-5, .00011))
+fig_3(nile_llm.df, ylim_0=(-375,300), xlim_0=(1865,1975),ylim_2=(-43,40), xlim_2=(1865,1975))
+fig_7(nile_llm.df, ylim_0=(-2.8,2.8), ylim_3=(-1,1), xlim_3=(.5,11))
+fig_8(nile_llm.df, ylim_0=(-3,2.2), ylim_2=(-3,2.2))
+
+# %%
+data2= pd.read_csv('../Data/AirPassengers.csv')
+# %%
