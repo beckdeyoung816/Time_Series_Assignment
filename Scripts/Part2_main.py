7ca079642b19e190dd00828280d24654ef1b053d
beckdeyoung816 <beckdeyoung@gmail.com>
2022-03-14 20:03:28 +0100

    Kalman filter and maximum likelihood

--- Scripts/Part2_main.py
+++ Scripts/Part2_main.py
@@ -4,18 +4,17 @@ import numpy as np
 import datetime as dt
 import scipy.stats as ss
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
-from SSM import SSM
+from SSM_SV import SSM_SV
 
 # %%
 # Load in the data
 sv = pd.read_excel('../Data/SvData.xlsx')
 sv.columns = ['r_t']
-
-# %%
 sv['y_t'] = sv['r_t'] / 100 # R_t is misscaled
 mu = np.mean(sv['y_t']) # Calculate the mean to detrend the data
-sv['x_t'] = np.log((sv['y_t']-mu) ** 2) # Transform returns
+sv['y_t'] = np.log((sv['y_t']-mu) ** 2) # Transform returns
 
 # %%
 plt.figure(figsize=(15,6))
@@ -28,3 +27,20 @@ plt.scatter(sv.index, sv.x_t)
 plt.ylim((-30, -8))
 plt.xlim((0,950))
 # %%
+
+test_llm = SSM_SV(data = sv[['y_t']], H_t = (np.pi ** 2) / 2, c_t = -1.27, theta0=[0.8,0.0077,0.995])
+
+# %%
+test_llm.kalman_filter()
+
+
+# %%
+plt.figure(figsize=(15,6))
+plt.plot(test_llm.df.y_t)
+plt.plot(test_llm.df.a_t, 'red')
+# %%
+
+mle_results = test_llm.mle_sv([0.8,0.0077,0.995])
+# %%
+
+
