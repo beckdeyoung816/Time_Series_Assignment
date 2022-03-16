9d28f32457e6a41bf5d6144bfe6376de902544dc
beckdeyoung816 <beckdeyoung@gmail.com>
2022-03-16 14:44:40 +0100

    Saving inital rv model

--- Scripts/Part2_main.py
+++ Scripts/Part2_main.py
@@ -56,40 +56,70 @@ axes[1].legend()
 # %%
 # Data prep
 rv = pd.read_csv('../Data/Realized_Volatility_Indices.csv')
-dji = rv[rv['Symbol'] == '.DJI'] # Select desired index
-dji = dji[['Date', 'close_price', 'rv5']]
+rv = rv[rv['Symbol'] == '.DJI'] # Select desired index
+rv = rv[['Date', 'close_price', 'rv5']]
 # Fix dates and filter for desired years
-dji['Date'] = pd.to_datetime(dji.Date) 
-dji['Date'] = dji['Date'].dt.date
-dji = dji[(dji['Date'] >= dt.date(2010,1,1)) & (dji['Date'] < dt.date(2016,1,1))].reset_index(drop=True)
+rv['Date'] = pd.to_datetime(rv.Date) 
+rv['Date'] = rv['Date'].dt.date
+rv = rv[(rv['Date'] >= dt.date(2010,1,1)) & (rv['Date'] < dt.date(2016,1,1))].reset_index(drop=True)
 
 # %%
-dji['y_t'] = np.log(dji.close_price / dji.close_price.shift(1))
-dji = dji.iloc[1:].reset_index(drop=True)
-mu = np.mean(dji['y_t']) # Calculate the mean to detrend the data
-dji['x_t'] = np.log((dji['y_t']-mu) ** 2) # Transform returns
+rv['y_t'] = np.log(rv.close_price / rv.close_price.shift(1))
+rv = rv.iloc[1:].reset_index(drop=True)
+mu = np.mean(rv['y_t']) # Calculate the mean to detrend the data
+rv['y_t'] = np.log((rv['y_t']-mu) ** 2) # Transform returns
+rv['X_t'] = np.log(rv['rv5'])
 
 # %%
-df_dji = dji[['x_t']].rename(columns={'x_t': 'y_t'}, copy=True)
-dji_ssm = SSM_SV(data = df_dji, H_t = (np.pi ** 2) / 2, c_t = -1.27, 
+df_rv = rv[['X_t', 'y_t']].copy(deep=True)
+rv_ssm = SSM_SV(data = df_rv, H_t = (np.pi ** 2) / 2, c_t = -1.27, 
                 # theta0=[0.8,0.0077,0.995]
                   Q_t = 0.4247156788750475,
                   d_t = -0.8739300473341086,
                   T_t = 0.9122642976429152
                  )
 # %%
-dji_ssm.kalman_filter()
-dji_ssm.state_smooth()
+rv_ssm.kalman_filter()
+rv_ssm.state_smooth()
 
 # %%
 fig, axes = plt.subplots(2,1, figsize=(15,12))
-axes[0].scatter(dji_ssm.df.index, dji_ssm.df.y_t, color='black', alpha=.5)
-axes[0].plot(dji_ssm.df.alpha_hat_t[2:], 'red', linewidth=2, label = 'Smoothed ht')
+axes[0].scatter(rv_ssm.df.index, rv_ssm.df.y_t, color='black', alpha=.5)
+axes[0].plot(rv_ssm.df.alpha_hat_t[2:], 'red', linewidth=2, label = 'Smoothed ht')
 axes[0].legend(loc='lower right')
 
 
 # xi = w/(1-phi) = -10.2089
 # Ht = ht - xi
-axes[1].plot(dji_ssm.df.a_t[4:] - dji_ssm.xi, 'blue', label = 'Filtered Ht')
-axes[1].plot(dji_ssm.df.alpha_hat_t[2:] - dji_ssm.xi, 'red', label = 'Smoothed Ht')
+axes[1].plot(rv_ssm.df.a_t[4:] - rv_ssm.xi, 'blue', label = 'Filtered Ht')
+axes[1].plot(rv_ssm.df.alpha_hat_t[2:] - rv_ssm.xi, 'red', label = 'Smoothed Ht')
 axes[1].legend()
+
+# %%
+dji_ssm_rv = SSM_SV(data = df_rv, H_t = (np.pi ** 2) / 2, c_t = -1.27, 
+                # theta0=[0.8,0.0077,0.995]
+                  Q_t = 0.4247156788750475,
+                  d_t = -0.8739300473341086,
+                  T_t = 0.9122642976429152,
+                  use_Beta=True
+                 )
+# %%
+dji_ssm_rv.kalman_filter()
+dji_ssm_rv.state_smooth()
+# %%
+fig, axes = plt.subplots(2,1, figsize=(15,12))
+axes[0].scatter(dji_ssm_rv.df.index, rv_ssm.df.y_t, color='black', alpha=.5)
+axes[0].plot(dji_ssm_rv.df.alpha_hat_t[2:], 'red', linewidth=2, label = 'Smoothed ht')
+axes[0].legend(loc='lower right')
+
+
+# xi = w/(1-phi) = -10.2089
+# Ht = ht - xi
+axes[1].plot(dji_ssm_rv.df.a_t[4:] - dji_ssm_rv.xi, 'blue', label = 'Filtered Ht')
+axes[1].plot(dji_ssm_rv.df.alpha_hat_t[2:] - dji_ssm_rv.xi, 'red', label = 'Smoothed Ht')
+axes[1].legend()
+
+# %%
+test = rv_ssm.df[['y_t', 'a_t', 'alpha_hat_t']]
+test[['X_t', 'a_beta_t', 'alpha_hat_beta_t']] = dji_ssm_rv.df[['X_t', 'a_t', 'alpha_hat_t']]
+# %%
--- Scripts/SSM_SV.py
+++ Scripts/SSM_SV.py
@@ -31,7 +31,7 @@ import scipy.stats as ss
 
 
 class SSM_SV:
-    def __init__(self, data:pd.DataFrame, H_t:float=None, c_t:float=None, Q_t:float=None, d_t:float=None,T_t:float=None,R_t:float=None, a_1:float=None, P_1:float=None, theta0:list=None):
+    def __init__(self, data:pd.DataFrame, H_t:float=None, c_t:float=None, Q_t:float=None, d_t:float=None,T_t:float=None,R_t:float=None, a_1:float=None, P_1:float=None, theta0:list=None, Beta:float=None, use_Beta:bool=False):
         """Initialize Local Level Model Object
         Args:
             data (pd.DataFrame): Time Series Data
@@ -42,7 +42,7 @@ class SSM_SV:
             q0 (list, optional): Starting value for q for maximum likelihood estimation of form [q0]
         """
         # Assign final attributes of the data and the number of observations    
-        self.df = data
+        self.df = data.copy(deep=True)
         self.N = self.df.shape[0]
         self.c_t = c_t
         self.H_t = H_t
@@ -50,7 +50,7 @@ class SSM_SV:
         # If no variances are specified, then we estimate them through maximum likelihood
         if Q_t is None or T_t is None or d_t is None:
             
-            mle_results = self.mle_sv(theta0) # Peform MLE
+            mle_results = self.qml_sv(theta0) # Peform MLE
             self.Q_t = mle_results[0] # Variance of Eta from MLE
             self.d_t = mle_results[1] # Omega from MLE
             self.T_t = mle_results[2] # Phi from MLE
@@ -61,7 +61,8 @@ class SSM_SV:
         
         self.R_t = np.sqrt(self.Q_t) # Calculate sigma_eta
         self.xi = self.d_t/(1-self.T_t) # Calculate xi
-            
+        self.d_t = self.d_t * np.ones(self.N)
+        
         # If starting values for P and a are not specified, we use diffuse initialization
         if a_1 is None or P_1 is None:
             # Diffuse Initialization
@@ -70,7 +71,11 @@ class SSM_SV:
         else:
             self.a_1 = a_1
             self.P_1 = P_1
-    
+
+        if use_Beta:
+            self.beta_df = data.copy(deep=True)
+            self.estimate_Beta()
+            
     def k_filter(self, y_t: float, a_t: float, P_t: float, H_t: float, Q_t: float, T_t, c_t: float, d_t: float, R_t: float):
         """Kalman Filter at a single point in time given values at time t-1
 
@@ -100,14 +105,15 @@ class SSM_SV:
 
         return a_tt, P_tt, a_t, P_t, v_t, F_t, K_t
     
-    def kalman_filter(self):
+    def kalman_filter(self, vars=['a_t','P_t', 'v_t','F_t','K_t'], beta=False,y='y_t'):
         """Kalman Filter for the entire time series
         """
+        df = self.beta_df if beta else self.df
         
-        self.df[['a_t','P_t', 'v_t','F_t','K_t']] = np.nan
+        df[vars] = np.nan
         
         # Loop through each observation of y_t
-        for t, y_t in enumerate(self.df['y_t']):
+        for t, y_t in enumerate(df[y]):
             # Initialize Values
             if t == 0 :
                 a_t = self.a_1 
@@ -115,10 +121,11 @@ class SSM_SV:
             
             # Apply the filter and save updated values for next iteration
             a_tt, P_tt, a_t, P_t, v_t, F_t, K_t = self.k_filter(y_t=y_t,a_t=a_t,P_t=P_t,H_t=self.H_t,
-                                                       Q_t=self.Q_t,T_t=self.T_t,c_t=self.c_t,d_t=self.d_t, R_t = self.R_t)
+                                                       Q_t=self.Q_t,T_t=self.T_t,c_t=self.c_t,d_t=self.d_t[t], R_t = self.R_t)
             
             # Store output
-            self.df.loc[t, ['a_t','P_t','v_t','F_t','K_t']] = [a_tt, P_tt, v_t, F_t, K_t]
+            
+            df.loc[t, vars] = [a_tt, P_tt, v_t, F_t, K_t]
 
     def state_smooth(self):
         
@@ -126,7 +133,7 @@ class SSM_SV:
         self.df['L_t'] = self.T_t - self.df['K_t']
         
         # Initialize columns for recursion
-        self.df[['r_t','N_t', 'alpha_hat_t', 'V_t']] = np.nan
+        self.df[['r_t','N_t', 'alpha_hat_t']] = np.nan
 
         # Smoothing goes in reverse (from t=n to t=1)
         for t in reversed(range(self.N)):
@@ -143,12 +150,8 @@ class SSM_SV:
         # These equations require r_{t-1} and N_{t-1} so we do not use the last values and start at first value
         self.df.loc[0, 'alpha_hat_t'] = self.a_1
         self.df.loc[1:,'alpha_hat_t'] = self.df.loc[1:, 'a_t'] + self.df.loc[1:, 'P_t'] * self.df.loc[:self.N, 'r_t']
-        
-        # Set first value then recurse
-        self.df.loc[0, 'V_t'] = self.P_1
-        self.df.loc[1:,'V_t'] = self.df.loc[1:, 'P_t'] - ((self.df.loc[1:, 'P_t'] ** 2) * self.df.loc[:self.N, 'N_t'])
     
-    def mle_sv(self, theta0:list):
+    def qml_sv(self, theta0:list):
         def calc_neg_log_lk(theta0:list):
             Q_t = theta0[0]
             d_t = theta0[1]
@@ -180,8 +183,19 @@ class SSM_SV:
         results = minimize(calc_neg_log_lk, theta0, method = 'L-BFGS-B', bounds = bnds) # Perform optimization
         
         return results['x'] # Return optimal q value
-    def smoothed_Ht(self):
-        self.df['H_star_t']
+    
+    def estimate_Beta(self):
+        self.kalman_filter(vars=['a_y_t','P_y_t','v_star_t','F_y_t','K_y_t'], beta=True, y='y_t')
+        
+        self.beta_df['X_t_plus_d_t'] = self.beta_df['X_t'] + self.d_t
+        self.kalman_filter(vars=['a_x_t','P_x_t', 'x_star_t','F_x_t','K_x_t'], beta=True, y='X_t_plus_d_t')
+        
+        self.Beta = np.sum(self.beta_df['x_star_t'] * self.beta_df['F_y_t'] * self.beta_df['v_star_t']) / \
+            np.sum(self.beta_df['x_star_t'] * self.beta_df['F_y_t'] * self.beta_df['x_star_t']) # Equation 6.2
+            
+        self.d_t = self.Beta * self.df['X_t'] + self.d_t
+        print(f'Beta: {self.Beta}')
+
     
     
     
